30f851d8c654b1c5e0d043353647bf93d3f98fdd
dollarkillerx <adapawang@gmail.com>
2022-08-22 00:03:51 +0800

    init project

---
+++ test/sr_test.go
@@ -0,0 +1,15 @@
+package test
+
+import "testing"
+
+var pk = `
+-----BEGIN RSA PUBLIC KEY-----
+MIGIAoGBAIMXLXQlTK912QbqLiBoC3wd8H1nni1tcLH9Qnv/KhYq4L8b17fju8aw
+eiN60HAtYWBCBWkH5akF+WVygej/4qgXCc+bf9pZwlkRqk818GdXs1AZux15+Mxr
+YJnUV/HbxVAoEvvbYsiWg8uZvM6GuQJwrN7uvvyDT4ZK8d3K78+RAgIQAQ==
+-----END RSA PUBLIC KEY-----
+`
+
+func TestRc(t *testing.T) {
+
+}
